23899303886b47dd81db52bb2d394976d463eb38
Michael Elliott <elliotecweb@gmail.com>
2013-12-13 16:31:39 -0700

    fixing heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ CodefellowsCrud::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://codefellows-crud.herokuapp.com/' }
 end
